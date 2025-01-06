97a00e6a4d2e084d272426e6d02afd7751944adf
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-01-06 19:01:20 +0900

    #11 clear

--- src/main/java/GoodPang/goodPang/service/ItemService.java
+++ src/main/java/GoodPang/goodPang/service/ItemService.java
@@ -6,14 +6,19 @@ import GoodPang.goodPang.repository.ItemRepository;
 import GoodPang.goodPang.response.exception.handler.ItemHandler;
 import GoodPang.goodPang.response.fail.ErrorStatus;
 import GoodPang.goodPang.web.dto.ItemRequestDto;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+
+    @Transactional(readOnly = true)
+    public Item getItem(Long itemId) {
+        return itemRepository.findById(itemId).orElseThrow(() -> new ItemHandler(ErrorStatus._ITEM_NOT_FOUND));
+    }
 
     @Transactional
     public Item addItem(ItemRequestDto.AddItemDto request) {
