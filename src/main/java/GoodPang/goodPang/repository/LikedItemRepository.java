b66f16ed56ae7b9e528f97219a68a29950fa293c
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-01-07 16:03:13 +0900

    #13 clear

--- src/main/java/GoodPang/goodPang/repository/LikedItemRepository.java
+++ src/main/java/GoodPang/goodPang/repository/LikedItemRepository.java
@@ -1,7 +1,11 @@
 package GoodPang.goodPang.repository;
 
 import GoodPang.goodPang.domain.item.LikedItem;
+import GoodPang.goodPang.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikedItemRepository extends JpaRepository<LikedItem, Long> {
+    List<LikedItem> findAllByMember(Member member);
 }
